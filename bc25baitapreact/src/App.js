1aca73f0c1f9fc12923500b5919795f823af83ee
tuyennguyen2021 <91683435+tuyennguyen2021@users.noreply.github.com>
2022-05-30 22:28:16 +0700

    update v1

---
+++ bc25baitapreact/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Glasses from "./baitap1";
+
+function App() {
+  return <Glasses />;
+}
+
+export default App;
---
+++ bc25baitapreact/src/baitap1/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Glasses extends Component {
+  render() {
+    return (
+      <div>
+        <h3>TRY GLASSES APP ONLINE</h3>
+        <button className="btn btn-success">tuyen</button>
+      </div>
+    );
+  }
+}
